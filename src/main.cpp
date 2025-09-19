86d5c15e2d6e52bacc65eb547713b1ede4d14afa
IrfanaliMeshania4 <ifumesh@gmail.com>
2025-09-19 17:14:25 -0500

    hi

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,105 @@
-
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_HTS221.h>
+#include <Adafruit_Sensor.h>
 #include <SPI.h>
 #include <SD.h>
 
 
-// SD Card CS pin (Chip Select)
-const int chipSelect = 5;
+
+
+
+
+
+
+const int chipSelect = 13;
+
+
+
+
+
+
+
+
+Adafruit_HTS221 hts = Adafruit_HTS221();
+
+
+ void initSDCard() {
+ 
+  pinMode(chipSelect, OUTPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite(chipSelect, HIGH);
+ 
+  SPI.begin(18, 19, 23, chipSelect);
+
+
+  delay(50);
+ 
+  if (!SD.begin(chipSelect, SPI, 4000000)) {
+    Serial.println("SD card initialization failed!");
+    while (1) { delay(10); }
+  }
+  Serial.println("SD card initialized.");
+}
+
+
+
+
+void logDataToSD(float temperature, float humidity) {
+  File dataFile = SD.open("/sensor_data.txt", FILE_APPEND);
+ 
+  if (dataFile) {
+    dataFile.print("Temperature: ");
+    dataFile.print(temperature);
+    dataFile.print(" C, Humidity: ");
+    dataFile.print(humidity);
+    dataFile.println(" %");
+    dataFile.close();
+    Serial.println("Data logged to SD card.");
+    digitalWrite(2, HIGH);
+  } else {
+    Serial.println("Error opening file for writing.");
+  }
+}
+
+
+
+
+
+
 
 
 void setup() {
-  // Start serial communication for debugging
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
 
-  // Initialize the SD card
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Initialization failed!");
-    return;
+
+  if (!hts.begin_I2C()) {
+    Serial.println("Couldn't find HTS221 sensor.");
+    while (1);  
   }
-  Serial.println("Initialization successful.");
-
-
-  // Open the file on the SD card (create it if it doesn't exist)
-  File dataFile = SD.open("/data.txt", FILE_WRITE);
-
-
-  // Check if the file is available
-  if (dataFile) {
-    Serial.println("Writing to data.txt...");
-    dataFile.println("Hello, this is ESP32 writing to SD card!");
-    dataFile.close();  // Close the file after writing
-    Serial.println("Write complete.");
-    digitalWrite(2, HIGH);
-  } else {
-    Serial.println("Error opening data.txt");
-  }
+  Serial.println("HTS221 sensor initialized.");
+ 
+  initSDCard();
 }
+
+
+
+
+
+
 
 
 void loop() {
-  // Do nothing here
+  sensors_event_t humidityEvent, tempEvent;
+  hts.getEvent(&humidityEvent, &tempEvent);  // fills both events
+
+
+  float temperature = tempEvent.temperature;                 // °C
+  float humidity    = humidityEvent.relative_humidity;       // %
+
+
+  logDataToSD(temperature, humidity);
+  delay(5000);
+  digitalWrite(2, LOW);
 }
+
+
